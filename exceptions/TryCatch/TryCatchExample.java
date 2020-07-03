608c2517291a47fc2c2c87c6ba6e8d800f7ccd44
Chandrabhatta Sriram <sriram.ch1995@gmail.com>
2020-07-04 00:22:50 +0530

    Excption Handling Example

---
+++ exceptions/TryCatch/TryCatchExample.java
@@ -0,0 +1,29 @@
+package exceptions.TryCatch;
+
+public class TryCatchExample {
+    public static void main(String[] args) {
+        try {
+            /** Uncomment the lines below to see the Exceptions being handled */
+
+            // System.out.println(10 / 0); // ArithmeticException
+            
+            int[] arr = { 1, 2, 4 };
+            // System.out.println(arr[10]); // ArrayIndexOutOfBoundsException
+
+            String str = "abc";
+            // System.out.println(str.charAt(10)); // StringIndexOutOfBoundsException
+
+            String s = null;
+            // System.out.println(s.charAt(10)); // NullPointerException
+
+            String numString = "ten";
+            System.out.println(Integer.parseInt(numString)); // NumberFormatException
+        } catch (ArithmeticException | NullPointerException | NumberFormatException ex) {
+            ex.printStackTrace(); // "java.lang.ArithmeticException: / by zero" at exceptions.TryCatchExample.main(TryCatchExample.java:6)
+            // System.out.println(ex); // same as "System.out.println(ex.toString());" // "java.lang.ArithmeticException: / by zero"
+            // System.out.println(ex.getMessage()); // "/ by zero"
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
