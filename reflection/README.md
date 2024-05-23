0a418c7ec4801d2a90206366326e797fa9b10e69
Chandrabhatta Sriram <chandrabhattasriram@Chandrabhattas-MacBook-Pro.local>
2024-05-23 05:52:08 +0530

    Added more details on Java Reflection API

--- reflection/README.md
+++ reflection/README.md
@@ -2,3 +2,12 @@
 
 - Used to know the capabilities of classes and methods during runtime.
 - Reflection API gices access to metadata, which data about classes and objects within JVM.
+
+### `java.lang.Class`
+
+- Not present inside the `java.lang.reflect` package, but is an essential package to understand.
+- This is the backbone of reflection. Find and load any class.
+- Whenever a class gets loaded by JVM, during that time, a `Class<class>` object gets generated for the class.
+- Three ways to instantiate a `Class<class>` object:
+  1. `Class::forName`: Takes in a `String` which is the fully qualified class name.
+     - `Class.forName("java.lang.String")` will create a `Class<String>` object.
--- reflection/ReflectionDemo/ReflectionDemo.java
+++ reflection/ReflectionDemo/ReflectionDemo.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 class MyClass {
-  private MyClass() {
+  public MyClass() {
     System.out.println("MyClass Object Created!");
   }
 
@@ -14,11 +14,12 @@ class MyClass {
 }
 
 public class ReflectionDemo {
-  public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+  public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException,
+      InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
     // MyClass myClass = new MyClass(); // error: MyClass() has private access in MyClass
 
     /**
-     * We can make use of `Class.forName` to create an object of the class as follows:
+     * 1. We can make use of `Class.forName` to create an object of the class as follows:
      */
 
     // Here, the fully qualified name of the class is passed as a string.
@@ -33,5 +34,44 @@ public class ReflectionDemo {
     // We can also typecase our object to MyClass directly
     MyClass myClassInstance = (MyClass) clazzConstructor.newInstance(); // MyClass Object Created!
     System.out.println(myClassInstance); // MyClass Object
+
+    /**
+     * 2. All class objects created from the same class will get the same object
+     */
+    Class<?> clazz1 = Class.forName("reflection.ReflectionDemo.MyClass");
+    Class<?> clazz2 = Class.forName("reflection.ReflectionDemo.MyClass");
+    System.out.println(clazz1 == clazz2); // true
+
+    Class<?> clazz3 = Class.forName("java.lang.String");
+    Class<?> clazz4 = Class.forName("java.lang.String");
+    System.out.println(clazz3 == clazz4); // true
+
+    /**
+     * 3. Using [ClassName].class to get the class object.
+     *    - We can use this for primitives as well, which will return the corresponding wrapper class.s
+     */
+
+    Class<?> clazz5 = int.class; // This will return the class object for intSSS
+    Class<?> clazz6 = String.class; // This will return the class object for String
+
+    /**
+     * 4. <instance>.getClass()
+     * 
+     * // myClassInstance.getClass(); // This will return the class object for MyClass
+     */
+    MyClass m = new MyClass();
+    Class<? extends MyClass> clazz7 = m.getClass(); // This will return the class object for MyClass
+    System.out.println(clazz7);
+
+    // To get superclasses
+    clazz7 = (Class<? extends MyClass>) clazz7.getSuperclass();
+
+    // To get the interfaces of the class
+    Class<?>[] interfaces = clazz7.getInterfaces();
+    System.out.println(interfaces);
+
+    // To get the name of the class
+    String className = clazz7.getName();
+    System.out.println(className); // reflection.ReflectionDemo.MyClass
   }
 }
